7ab6f66000be61a2ebe82a13a57a888710c8c62f
Micheal Nestor <michealnestor@outlook.com>
2023-01-10 13:37:45 +0000

    Added the header component

---
+++ src/components/ui/Popup.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { motion } from "framer-motion";
+
+const Popup = (props) => {
+  const unmountedState = { opacity: 0, top: "-10%" };
+  const mountedState = { opacity: 1, top: "5%" };
+  return (
+    <motion.div
+      key="popup"
+      initial={unmountedState}
+      animate={mountedState}
+      exit={unmountedState}
+      transition={{ duration: 1, type: "spring" }}
+      className="popup"
+    >
+      <h1 className="popup__title">{props.title}</h1>
+      <p className="popup__message">{props.message}</p>
+    </motion.div>
+  );
+};
+
+export default Popup;
