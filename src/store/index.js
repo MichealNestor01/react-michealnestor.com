bc837722b79322fabfa6ff8219ee3767280b7653
Micheal Nestor <michealnestor@outlook.com>
2023-01-10 13:41:51 +0000

    Added redux store

---
+++ src/store/index.js
@@ -0,0 +1,55 @@
+import { createSlice, configureStore } from "@reduxjs/toolkit";
+
+// development = http://localhost:3000/; deployment = https://www.michealnestor.com;
+export const domainName = "http://localhost:3000/";
+
+/*--------------------------------------------------------------------------*/
+/*-----------------------UI SLICE-------------------------------------------*/
+/*--------------------------------------------------------------------------*/
+
+const initalUiState = {
+  showPopup: false,
+  popupTitle: "",
+  popupMessage: "",
+  screenWidth: 1920,
+};
+
+const uiSlice = createSlice({
+  name: "ui",
+  initialState: initalUiState,
+  reducers: {
+    show(state, action) {
+      state.showPopup = true;
+      state.popupTitle = action.payload.title;
+      state.popupMessage = action.payload.message;
+    },
+    hidePopup(state) {
+      state.showPopup = false;
+    },
+    setScreenWidth(state, action) {
+      state.screenWidth = action.payload;
+    },
+  },
+});
+
+const uiShowPopup = (title, message) => {
+  return async (dispatch) => {
+    dispatch(uiActions.show({ title, message }));
+    await new Promise((resolve) => setTimeout(resolve, 3000));
+    dispatch(uiActions.hidePopup());
+  };
+};
+
+/*--------------------------------------------------------------------------*/
+/*--------------------STORE CONFIG------------------------------------------*/
+/*--------------------------------------------------------------------------*/
+
+const store = configureStore({
+  reducer: {
+    ui: uiSlice.reducer,
+  },
+});
+
+export const uiActions = { showPopup: uiShowPopup, ...uiSlice.actions };
+
+export default store;
