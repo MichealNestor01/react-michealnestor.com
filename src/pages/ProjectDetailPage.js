d1c8b80f61337ce4521a427dc694b1634bcca16f
Micheal Nestor <michealnestor@outlook.com>
2023-03-04 14:00:06 +0000

    project detail page is now scrollable

--- src/pages/ProjectDetailPage.js
+++ src/pages/ProjectDetailPage.js
@@ -58,56 +58,58 @@ const ProjectDetailPage = () => {
         <meta name="description" content={`Click to read about Micheal Nestor's ${title} project.`} />
       </Helmet>
       <section className="projectDetailPage">
-        <section className="header">
-          <h1 className="title">{title}</h1>
-          <section className="details">
-            <h2 className="detail">Completed: {dateString}</h2>
-            <h2 className="detail">Project Type: {project_type}</h2>
-          </section>
-        </section>
-        <section className="main">
-          <section className="galleryContainer">
-            <PhotoGallery photos={photosList} />
-          </section>
-          <section className="descriptionContainer">
-            <p className="description">{description}</p>
-            <section className="buttons">
-              {project_type === "Javascript" && (
-                <motion.a
-                  variants={buttonVariants}
-                  whileTap="tap"
-                  whileHover="hover"
-                  href={`${domainName}projects/${slug}`}
-                  target="_blank"
-                  className="button globalButton runProjectButton"
-                >
-                  Run Project
-                </motion.a>
-              )}
-              {source_code && (
-                <motion.a
-                  variants={buttonVariants}
-                  whileTap="tap"
-                  whileHover="hover"
-                  href={source_code}
-                  target="_blank"
-                  className="button globalButton"
-                >
-                  Source Code
-                </motion.a>
-              )}
+        <wrapper>
+          <section className="header">
+            <h1 className="title">{title}</h1>
+            <section className="details">
+              <h2 className="detail">Completed: {dateString}</h2>
+              <h2 className="detail">Project Type: {project_type}</h2>
             </section>
           </section>
-        </section>
-        <motion.div
-          variants={buttonVariants}
-          whileTap="tap"
-          whileHover="hover"
-          onClick={() => navigate(`/portfolio`)}
-          className="backButton globalButton"
-        >
-          Other Projects
-        </motion.div>
+          <section className="main">
+            <section className="galleryContainer">
+              <PhotoGallery photos={photosList} />
+            </section>
+            <section className="descriptionContainer">
+              <p className="description">{description}</p>
+              <section className="buttons">
+                {project_type === "Javascript" && (
+                  <motion.a
+                    variants={buttonVariants}
+                    whileTap="tap"
+                    whileHover="hover"
+                    href={`${domainName}projects/${slug}`}
+                    target="_blank"
+                    className="button globalButton runProjectButton"
+                  >
+                    Run Project
+                  </motion.a>
+                )}
+                {source_code && (
+                  <motion.a
+                    variants={buttonVariants}
+                    whileTap="tap"
+                    whileHover="hover"
+                    href={source_code}
+                    target="_blank"
+                    className="button globalButton"
+                  >
+                    Source Code
+                  </motion.a>
+                )}
+              </section>
+            </section>
+          </section>
+          <motion.div
+            variants={buttonVariants}
+            whileTap="tap"
+            whileHover="hover"
+            onClick={() => navigate(`/portfolio`)}
+            className="backButton globalButton"
+          >
+            Other Projects
+          </motion.div>
+        </wrapper>
       </section>
     </Fragment>
   );
